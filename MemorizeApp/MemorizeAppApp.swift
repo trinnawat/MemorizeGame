a1bd70d3541ae6c9cf1fadf6b6d3fe4e7fd82942
Trinnawat Ch <trinnawat.c@tcrbank.com>
2025-03-10 18:43:01 +0700

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Trinnawat.c on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
